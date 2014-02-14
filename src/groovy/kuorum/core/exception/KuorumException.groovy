dd13825ad6bcb1174924f08bf9272a23f65d59af
Iñaki <inaki.dominguez@kuorum.org>
2014-02-14 06:03:17 +0100

    Excepciones básicas de kuorum

---
+++ src/groovy/kuorum/core/exception/KuorumException.groovy
@@ -0,0 +1,40 @@
+package kuorum.core.exception
+
+import org.codehaus.groovy.grails.exceptions.GrailsException
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: iduetxe
+ * Date: 5/04/13
+ * Time: 19:15
+ * To change this template use File | Settings | File Templates.
+ */
+class KuorumException extends GrailsException{
+
+    ArrayList<KuorumExceptionData> errors
+
+    public KuorumException(String message, KuorumExceptionData error){
+        super(message)
+        this.errors = new ArrayList<KuorumExceptionData>();
+        this.errors.add(error)
+    }
+    public KuorumException(String message, ArrayList<KuorumExceptionData> errors){
+        super(message)
+        this.errors = errors
+    }
+    public KuorumException(String message){
+        super(message)
+        this.errors = new ArrayList<KuorumExceptionData>()
+    }
+
+    public KuorumException(KuorumExceptionData error){
+        super(error.code)
+        this.errors = new ArrayList<KuorumExceptionData>();
+        this.errors.add(error)
+    }
+
+
+    public void addError(KuorumExceptionData error){
+        this.errors.add(error)
+    }
+}
