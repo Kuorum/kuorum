baa6e37540dcdcb28759983421732107be0fd710
Iñaki <inaki.dominguez@kuorum.org>
2014-02-14 06:07:54 +0100

    Paquetería para los command y el primer command

---
+++ src/groovy/kuorum/web/commands/PostCommand.groovy
@@ -0,0 +1,16 @@
+package kuorum.web.commands
+
+import kuorum.core.model.PostType
+import kuorum.law.Law
+
+/**
+ * Created by iduetxe on 13/02/14.
+ */
+class PostCommand {
+
+    Law law
+    String title
+    String text
+    String photo
+    PostType postType
+}
