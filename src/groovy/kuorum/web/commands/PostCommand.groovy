c7de7b21b5573bb62b5411b8efb16777d72a82ae
Iñaki <iduetxe@gmail.com>
2014-02-18 20:00:18 +0100

    Tag validatable

--- src/groovy/kuorum/web/commands/PostCommand.groovy
+++ src/groovy/kuorum/web/commands/PostCommand.groovy
@@ -1,11 +1,13 @@
 package kuorum.web.commands
 
+import grails.validation.Validateable
 import kuorum.core.model.PostType
 import kuorum.law.Law
 
 /**
  * Created by iduetxe on 13/02/14.
  */
+@Validateable
 class PostCommand {
 
     Law law
