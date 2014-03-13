1fa06f537a6554ff5931f0bf08d4617c00061a4e
Iñaki <iduetxe@gmail.com>
2014-03-13 16:42:58 +0100

    Deleting temporal files of an user

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -2,13 +2,14 @@ includeTargets << grailsScript("_GrailsInit")
 
 
 eventConfigureTomcat = {tomcat ->
+    def path = "/home/iduetxe/kuorum/kuorum/images"
     System.out.print("####EVENT: Imagenes en : /uploadedImages");
     def ctx=tomcat.host.findChild(serverContextPath)
     if ( ctx.aliases ) {
-        ctx.aliases += ',/uploadedImages=/home/iduetxe/kuorum/kuorum/images'
+        ctx.aliases += ",/uploadedImages=${path}"
         System.out.print("####Added");
     } else {
         System.out.println("####New");
-        ctx.aliases = '/uploadedImages=/home/iduetxe/kuorum/kuorum/images'
+        ctx.aliases = "/uploadedImages=${path}"
     }
 }
