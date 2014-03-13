cb26471b962207e1141d1127b1bcbf6bf0b0f63a
Iñaki <iduetxe@gmail.com>
2014-03-13 17:59:39 +0100

    Añadido enviroment para que solo funcione en desarrollo

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,15 +1,21 @@
+import grails.util.Environment
+
 includeTargets << grailsScript("_GrailsInit")
 
 
 eventConfigureTomcat = {tomcat ->
-    def path = "/home/iduetxe/kuorum/kuorum/images"
-    System.out.print("####EVENT: Imagenes en : /uploadedImages");
-    def ctx=tomcat.host.findChild(serverContextPath)
-    if ( ctx.aliases ) {
-        ctx.aliases += ",/uploadedImages=${path}"
-        System.out.print("####Added");
-    } else {
-        System.out.println("####New");
-        ctx.aliases = "/uploadedImages=${path}"
+
+    if(Environment.current == Environment.DEVELOPMENT) {
+        def path = "/home/iduetxe/kuorum/kuorum/images"
+        System.out.print("####EVENT: Imagenes en : /uploadedImages");
+        def ctx=tomcat.host.findChild(serverContextPath)
+        if ( ctx.aliases ) {
+            ctx.aliases += ",/uploadedImages=${path}"
+            System.out.print("####Added");
+        } else {
+            System.out.println("####New");
+            ctx.aliases = "/uploadedImages=${path}"
+        }
+
     }
 }
