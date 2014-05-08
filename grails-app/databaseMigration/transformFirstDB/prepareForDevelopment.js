221cff77c522d2a616456ddd7ffcc62972445eb8
Iñaki <iduetxe@gmail.com>
2014-05-08 09:12:32 +0200

    js de bBDD para desarrollo poner género

--- grails-app/databaseMigration/transformFirstDB/prepareForDevelopment.js
+++ grails-app/databaseMigration/transformFirstDB/prepareForDevelopment.js
@@ -5,6 +5,8 @@ load("htmlDecoder.js")
 db.kuorumUser.update({},{$set:{"password" : "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"}},{"upsert":false, "multi":true}) //test
 
 var madrid = db.region.find({"iso3166_2" : "EU-ES-MD-MD"})[0]
+
+//PERSONAL DATA
 db.kuorumUser.update(
     {"personalData.postalCode":{$exists:1}, userType:{$ne:"ORGANIZATION"}},
     {$set:{
@@ -14,6 +16,22 @@ db.kuorumUser.update(
         "personalData.birthday":new Date(),
         "personalData.studies":"NONE",
         "personalData.workingSector":"OTHERS"
+    }},
+    {"upsert":false, "multi":true}) //test
+
+//SET GENDER IF IS NOT SET
+db.kuorumUser.update(
+    {"personalData.gender":{$exists:0}, userType:{$ne:"ORGANIZATION"}},
+    {$set:{
+        "personalData.gender":"MALE"
+    }},
+    {"upsert":false, "multi":true}) //test
+
+//SET GENDER IF IS NOT SET AND IS AN ORGANIZATION
+db.kuorumUser.update(
+    {"personalData.gender":{$exists:0}, userType:"ORGANIZATION"},
+    {$set:{
+        "personalData.gender":"ORGANIZATION"
     }},
     {"upsert":false, "multi":true}) //test
 
