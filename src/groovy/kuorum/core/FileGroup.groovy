22e651d56775be7025e47486063344c5d6606c8a
Iñaki <iduetxe@gmail.com>
2014-05-12 16:20:51 +0200

    User profile image

--- src/groovy/kuorum/core/FileGroup.groovy
+++ src/groovy/kuorum/core/FileGroup.groovy
@@ -5,6 +5,7 @@ package kuorum.core
 public enum FileGroup {
 
     USER_AVATAR("UsersFiles", 1024 * 1000 /*10 MB */, '1', 110),
+    USER_PROFILE("UsersFiles", 1024 * 1000 /*10 MB */, '287/100', 110),
     LAW_IMAGE("LawsFiles", 1024000 /*10 MB */, '123/62', 738),
     POST_IMAGE("PoliticianFiles", 1024000 /*10 MB */,'123/62',738)
 
