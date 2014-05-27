7fb2b02487c75ddc6049a26465fc55294be9a1a5
Iñaki <iduetxe@gmail.com>
2014-05-27 10:48:14 +0200

    Integración con Abby

--- web-app/js/custom.js
+++ web-app/js/custom.js
@@ -46,100 +46,13 @@ $(document).tooltip({
 
 $(document).ready(function() {
 
+
+    // inicializa formato fechas
     $("time.timeago").timeago();
-
-    $(function(){
-		var votesdata = [
-			// A favor
-			{
-				value: 1498,
-				color:"#93bfdb"
-			},
-			// En contra
-			{
-				value : 1687,
-				color : "#213b47"
-			},
-			// Abstenciones
-			{
-				value : 1978,
-				color : "#d1d1d1"
-			}
-
-		];
-		var globalGraphSettings = {
-			animation : Modernizr.canvas, // si el navegador no soporta canvas deshabilito animación
-			segmentShowStroke : false,
-			percentageInnerCutout : 60
-		};
-        //////ABBBYYYY ESTO NO ME FUNCIONA!!!!!!!!!!!
-//		var ctx = $("#votesChart").get(0).getContext("2d");
-//		var myDoughnut = new Chart(ctx);
-//		new Chart(ctx).Doughnut(votesdata, globalGraphSettings);
-	});
-
-
-	// inicializa mapa
-//	$(function(){
-//		$.getJSON('/kuorum/js/data-map.json', function(data){
-//		new jvm.WorldMap({
-//			map: 'spain',
-//			backgroundColor: '#ffffff',
-//			zoomOnScroll: false,
-//			regionsSelectable: true,
-//			regionsSelectableOne: true,
-//			regionStyle: {
-//				initial: {
-//					fill: '#EFEFEF',
-//					"fill-opacity": 1,
-//					stroke: 'yes',
-//					"stroke-width": 3,
-//					"stroke-opacity": 1
-//				},
-//				hover: {
-//					fill: '#ff9431',
-//					"fill-opacity": 0.7
-//				},
-//				selected: {
-//					fill: '#ff9431',
-//					"fill-opacity": 1
-//				},
-//				selectedHover: {
-//					fill: '#ff9431',
-//					"fill-opacity": 1
-//				}
-//			},
-//		    container: $('#map'),
-//		    	series: {
-//		    		regions: [{
-//		          	scale: {
-//		          		'1': '#93bfdb',
-//		          		'2': '#213b47',
-//		            	'3': '#d1d1d1'
-//		          	},
-//		          	attribute: 'fill',
-//		          	values: data['votation'].results
-//		    	}]
-//		    },
-//		    onRegionSelected: function(event, selectedRegion){
-//				$('.kakareo.post.ley.ficha > h2 > span').text(selectedRegion);
-//		    }
-//		   });
-//
-//		});
-//
-//		$('#allMap').on('click', function (e) {
-//			e.preventDefault();
-//			var map = $('#map').vectorMap('get', 'mapObject');
-//			map.clearSelectedRegions();
-//			$('.kakareo.post.ley.ficha > h2 > span').text('España');
-//		});
-//
-//	});
 
 
 	// slider de las imágenes de fondo en la Home
-	$('.home .images').bgswitcher({
+	$(".home .images").bgswitcher({
 		duration: 2000,
 	  	images: ['images/home1.jpg', 'images/home2.jpg', "images/home3.jpg"]
 	});
@@ -210,7 +123,9 @@ $(document).ready(function() {
 	//tooltip visible sobre la progress bar
 	$('.progress-bar').tooltip({trigger: 'manual', placement: 'top'}).tooltip('show');
 
-	$('body').on('click', '#karma .close', function() {
+
+    // cierre de la ventana del Karma
+    $('body').on('click', '#karma .close', function() {
 		karma.close()
  	});
 
