8507b3b2c0232ed6d7b01cbbf2b0e8568f3de06e
Iñaki <iduetxe@gmail.com>
2014-06-02 12:07:54 +0200

    Tamaño minimo de las imágenes a 100K

--- src/groovy/kuorum/core/FileGroup.groovy
+++ src/groovy/kuorum/core/FileGroup.groovy
@@ -7,7 +7,9 @@ public enum FileGroup {
     USER_AVATAR("UsersFiles", 1024 * 1000 /*10 MB */, '1', 110),
     USER_PROFILE("UsersFiles", 1024 * 1000 /*10 MB */, '287/100', 110),
     LAW_IMAGE("LawsFiles", 1024000 /*10 MB */, '123/62', 738),
-    POST_IMAGE("PoliticianFiles", 1024000 /*10 MB */,'123/62',738)
+    POST_IMAGE("PoliticianFiles", 1024000 /*10 MB */,'123/62',738);
+
+    static final Long MIN_SIZE_IMAGE = 1024 * 100
 
     String folderPath
     Long maxSize
