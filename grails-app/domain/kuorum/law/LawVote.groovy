4d35ce6ea3675173eb80e47ead0ca1a02ece9916
Iñaki <iduetxe@gmail.com>
2014-06-03 17:18:54 +0200

    Mierdaaaaaa buuuuuuuuggggggggggg!!!!!!!!!!!

--- grails-app/domain/kuorum/law/LawVote.groovy
+++ grails-app/domain/kuorum/law/LawVote.groovy
@@ -3,8 +3,10 @@ package kuorum.law
 import kuorum.core.model.VoteType
 import kuorum.users.KuorumUser
 import kuorum.users.PersonalData
+import org.bson.types.ObjectId
 
 class LawVote {
+    ObjectId id
     KuorumUser kuorumUser
     PersonalData personalData
     Law law
