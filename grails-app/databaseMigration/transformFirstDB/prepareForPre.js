f24bea7d0aa345505f4ba3ca26364080b98f55d4
Iñaki <iduetxe@gmail.com>
2014-06-04 11:07:28 +0200

    prepareForPre fallaba

--- grails-app/databaseMigration/transformFirstDB/prepareForPre.js
+++ grails-app/databaseMigration/transformFirstDB/prepareForPre.js
@@ -1,6 +1,8 @@
 
-dbOrigin = dbOrigin || connect("localhost:27017/KuorumWeb");
-dbDest = dbDest || connect("localhost:27017/Kuorum");
+var dbOrigin = dbOrigin || connect("localhost:27017/KuorumWeb");
+var dbDest = dbDest || connect("localhost:27017/Kuorum");
+
+load("htmlDecoder.js")
 
 dbDest.kuorumUser.update({},{$set:{"password" : "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"}},{"upsert":false, "multi":true}) //test
 
