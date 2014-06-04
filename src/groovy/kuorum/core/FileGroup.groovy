5127a12e8175e3a1d914e058b4d4c0d580957d27
Iñaki <iduetxe@gmail.com>
2014-06-04 15:28:36 +0200

    Cambiado tamaño imagenes permitido

--- src/groovy/kuorum/core/FileGroup.groovy
+++ src/groovy/kuorum/core/FileGroup.groovy
@@ -4,10 +4,10 @@ package kuorum.core
  */
 public enum FileGroup {
 
-    USER_AVATAR("UsersFiles", 1024 * 1000 /*10 MB */, '1', 110),
-    USER_PROFILE("UsersFiles", 1024 * 1000 /*10 MB */, '287/100', 110),
-    LAW_IMAGE("LawsFiles", 1024000 /*10 MB */, '123/62', 738),
-    POST_IMAGE("PoliticianFiles", 1024000 /*10 MB */,'123/62',738);
+    USER_AVATAR("UsersFiles", 1024 * 1000 *8 /*1 MB */, '1', 110),
+    USER_PROFILE("UsersFiles", 1024 * 1000 *8/*1 MB */, '287/100', 110),
+    LAW_IMAGE("LawsFiles", 1024 * 1000 *10 /*10 MB */, '123/62', 738),
+    POST_IMAGE("PoliticianFiles", 1024 *1000 *8 /*8 MB */,'123/62',738);
 
     static final Long MIN_SIZE_IMAGE = 1024 * 10
 
