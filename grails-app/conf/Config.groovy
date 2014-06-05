0fcf417d1a1c27e7944ababa0ef53aecb7ec52d0
Iñaki <iduetxe@gmail.com>
2014-06-05 13:16:09 +0200

    Config sin entronos.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -12,12 +12,38 @@ import kuorum.core.model.gamification.GamificationElement
 //                             "file:${userHome}/.grails/${appName}-config.groovy"]
 
 grails.config.locations = [
-        "classpath:${grails.util.Environment.current.name}_config.properties",
-        "classpath:Config_${grails.util.Environment.current.name}.groovy"
+        "classpath:${grails.util.Environment.current.name}_config.properties"
+//        "classpath:Config_${grails.util.Environment.current.name}.groovy"
 //        "file:/home/iduetxe/kuorum/kuorum/grails-app/conf/${grails.util.Environment.current.name}_milestones.groovy"
 //        Environment.current.name
 //        "classpath:development_config.properties"
 ]
+
+// log4j configuration
+
+grails.logging.jul.usebridge = false
+grails.serverURL = "http://127.0.0.1:8080/kuorum"
+
+log4j = {
+    appenders {
+        console name:'stdout', layout:pattern(conversionPattern: '%d{yyyy MM dd HH:mm:ss,SSS} [%c] # %-5p %m  %n' )
+    }
+
+    info    'grails.app'
+
+    error  'org.codehaus.groovy.grails.web.servlet',        // controllers
+            'org.codehaus.groovy.grails.web.pages',          // GSP
+            'org.codehaus.groovy.grails.web.sitemesh',       // layouts
+            'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+            'org.codehaus.groovy.grails.web.mapping',        // URL mapping
+            'org.codehaus.groovy.grails.commons',            // core / classloading
+            'org.codehaus.groovy.grails.plugins',            // plugins
+            'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
+            'org.springframework',
+            'org.hibernate',
+            'net.sf.ehcache.hibernate'
+}
+
 
 // if (System.properties["${appName}.config.location"]) {
 //    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
