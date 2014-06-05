c5418fc90d46f2d6c63e0097122625f62b2af1fc
Iñaki <iduetxe@gmail.com>
2014-06-05 18:06:02 +0200

    Validación twttier

--- src/groovy/kuorum/web/commands/profile/SocialNetworkCommand.groovy
+++ src/groovy/kuorum/web/commands/profile/SocialNetworkCommand.groovy
@@ -14,7 +14,7 @@ class SocialNetworkCommand {
 
     static constraints = {
         facebook nullable:true, url:true
-        twitter nullable:true
+        twitter nullable:true, matches: '@[a-zA-Z0-9_]+'
         blog nullable:true, url:true
         googlePlus nullable:true, url:true
     }
