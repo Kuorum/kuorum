123f31c742562f2460ee0a1218832687d28b6ae1
Iñaki <iduetxe@gmail.com>
2014-06-22 11:15:51 +0200

    BUGS-102 32 políticos

--- grails-app/controllers/kuorum/DashboardController.groovy
+++ grails-app/controllers/kuorum/DashboardController.groovy
@@ -58,9 +58,9 @@ class DashboardController {
     def discover(){
         List<Law> relevantLaws = lawService.relevantLaws(new Pagination(max:5))
         List<Law> recommendedLaws = lawService.recommendedLaws(new Pagination(max:3))
-        List<KuorumUser> mostActiveUsers = kuorumUserService.mostActiveUsersSince(new Date() -7, new Pagination(max:25))
-        List<KuorumUser> bestSponsors = kuorumUserService.bestSponsorsSince(new Date() -7, new Pagination(max:20))
-        List<KuorumUser> bestPoliticians = kuorumUserService.bestPoliticiansSince(new Date() -7, new Pagination(max:20))
+        List<KuorumUser> mostActiveUsers = kuorumUserService.mostActiveUsersSince(new Date() -7, new Pagination(max:24))
+        List<KuorumUser> bestSponsors = kuorumUserService.bestSponsorsSince(new Date() -7, new Pagination(max:24))
+        List<KuorumUser> bestPoliticians = kuorumUserService.bestPoliticiansSince(new Date() -7, new Pagination(max:32))
 //        log.debug("discover")
         [
                 relevantLaws:relevantLaws,
