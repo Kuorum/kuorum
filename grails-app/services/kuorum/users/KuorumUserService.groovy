82c19e2eb2e8d802029d0c1817d3811773443385
Iñaki <iduetxe@gmail.com>
2014-07-03 17:46:18 +0200

    BUGS-77 No se modificaba el email del usuario eliminado

--- grails-app/services/kuorum/users/KuorumUserService.groovy
+++ grails-app/services/kuorum/users/KuorumUserService.groovy
@@ -373,6 +373,7 @@ class KuorumUserService {
         String nombreEmail = user.email.split("@")[0]
         String domain = user.email.split("@")[1]
         String email = "BORRADO_${nombreEmail}@NO_EMAIL_${domain}"
+        user.email = email
         if (!user.save(flush: true)) {
             //TODO: Gestion errores
             log.error("Error salvando usuario ${user.id}. ERRORS => ${user.errors}")
