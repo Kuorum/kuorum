926e78a4c844964033f9e40b3612255638af11d0
Iñaki <iduetxe@gmail.com>
2014-07-22 19:48:52 +0200

    BUGS-135 He tenido que cargar la progres bar 2 veces. Es una ñapa pero ahí va.

--- web-app/js/custom.js
+++ web-app/js/custom.js
@@ -145,7 +145,7 @@ $(document).ready(function() {
 		//Default is 75px, set to 0 for demo so any distance triggers swipe
 		threshold:0
 	});
-
+    setTimeout(prepareProgressBar, 500)
     prepareProgressBar();
 
 	//tooltip visible sobre la progress bar
