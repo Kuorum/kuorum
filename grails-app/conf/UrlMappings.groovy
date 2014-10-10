0a2e02c4a6a5930df3ef4f3f6604a296fb2d67eb
Iñaki <iduetxe@gmail.com>
2014-10-10 21:26:23 +0200

    Urls iguales tanto para admin como para politico

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -172,9 +172,6 @@ class UrlMappings {
         name adminCreateInstitution:"/admin/instituciones/crear"    (controller:"adminRegion"){action =[GET:"createInstitution", POST:"saveInstitution"]}
         name adminEditInstitution:  "/admin/instituciones/editar"   (controller:"adminRegion"){action =[GET:"editInstitution", POST:"saveInstitution"]}
 
-        name politicianIndex:       "/gestion/$regionName"                  (controller:"adminLaw", action: "index")
-        name politicianCreateLaw:   "/gestion/$regionName/iniciativas/crear"(controller:"adminLaw"){action =[GET:"createLaw", POST:"saveLaw"]}
-
 
         "/sitemap"{
             controller = 'siteMap'
