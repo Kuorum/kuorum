748467ac3b2b671064a215b88570150522728f78
Iñaki <iduetxe@gmail.com>
2014-10-17 19:27:12 +0200

    Para borrar los clucks

--- grails-app/databaseMigration/transformFirstDB/deleteUser.js
+++ grails-app/databaseMigration/transformFirstDB/deleteUser.js
@@ -7,3 +7,8 @@ dbDest.kuorumUser.remove({_id:userId});
 
 dbDest.kuorumUser.update({followers:userId},{$pull:{followers:userId}},{multi:true})
 dbDest.kuorumUser.update({following:userId},{$pull:{following:userId}},{multi:true})
+
+dbDest.cluck.remove({owner:userId})
+dbDest.cluck.remove({defendedBy:userId})
+
+//TODO SI TIENE POST ES UNA MIERDA QUE NO ESTA HECHA
