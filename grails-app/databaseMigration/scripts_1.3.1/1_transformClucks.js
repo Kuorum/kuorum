40fec5da0a5b99f0a7ac48bf93fedac44a006853
Iñaki <iduetxe@gmail.com>
2014-10-26 18:44:21 +0100

    BUGS-189 - El cluck de un usuario va evolucionando a medida que realiza acciones

--- grails-app/databaseMigration/scripts_1.3.1/1_transformClucks.js
+++ grails-app/databaseMigration/scripts_1.3.1/1_transformClucks.js
@@ -35,3 +35,29 @@ dbDest.cluck.find().forEach(function(cluck){
     }
     dbDest.cluck.remove({_id:cluck._id})
 });
+
+//DELETING REPETEATED CLUCKS
+
+dbDest.cluck.aggregate(
+    [
+        {$group:{
+            _id:{post:'$post',owner:'$owner'},
+               lastUpdated:{$max:"$lastUpdated"},
+            total:{$sum:1}}
+        },
+        {$match:{
+            total:{$gt:1}}
+        }
+    ]).forEach(function(group){
+        var cluckToSave = dbDest.cluck.find({
+            post:group._id.post,
+            owner: group._id.owner,
+            lastUpdated:group.lastUpdated
+        })[0]
+        dbDest.cluck.remove({
+            post:group._id.post,
+            owner: group._id.owner
+        })
+        cluckToSave.cluckAction = "DEBATE"
+        dbDest.cluck.save(cluckToSave);
+    })
