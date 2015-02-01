2decce9dbaf2f5d7a8169d81a5d3e7f4c99695f5
Iñaki <iduetxe@gmail.com>
2015-02-01 18:10:25 +0100

    Estaba mal el script

--- grails-app/databaseMigration/scripts_1.3.6/1_refactorLaw.js
+++ grails-app/databaseMigration/scripts_1.3.6/1_refactorLaw.js
@@ -7,9 +7,9 @@ dbDest.lawClosedNotification.renameCollection("projectClosedNotification");
 dbDest.lawVote.renameCollection("projectVote");
 dbDest.kuorumFile.update({fileGroup: 'LAW_IMAGE'},{$set:{fileGroup:'PROJECT_IMAGE'}}, {multi:true});
 
-db.kuorumUser.update({"avatar.fileGroup":"LAW_IMAGE"},{$set:{"avatar.fileGroup":"PROJECT_IMAGE"}},{multi:true} );
-db.kuorumUser.update({"imageProfile.fileGroup":"LAW_IMAGE"},{$set:{"imageProfile.fileGroup":"PROJECT_IMAGE"}},{multi:true} );
-db.post.update({},{$rename:{'law':'project'}}, {multi:true});
-db.post.update({"multimedia.fileGroup":"LAW_IMAGE"},{$set:{"multimedia.fileGroup":"PROJECT_IMAGE"}},{multi:1} );
-db.project.update({"image.fileGroup":"LAW_IMAGE"},{$set:{"image.fileGroup":"PROJECT_IMAGE"}},{multi:1} );
+dbDest.kuorumUser.update({"avatar.fileGroup":"LAW_IMAGE"},{$set:{"avatar.fileGroup":"PROJECT_IMAGE"}},{multi:true} );
+dbDest.kuorumUser.update({"imageProfile.fileGroup":"LAW_IMAGE"},{$set:{"imageProfile.fileGroup":"PROJECT_IMAGE"}},{multi:true} );
+dbDest.post.update({},{$rename:{'law':'project'}}, {multi:true});
+dbDest.post.update({"multimedia.fileGroup":"LAW_IMAGE"},{$set:{"multimedia.fileGroup":"PROJECT_IMAGE"}},{multi:1} );
+dbDest.project.update({"image.fileGroup":"LAW_IMAGE"},{$set:{"image.fileGroup":"PROJECT_IMAGE"}},{multi:1} );
 
