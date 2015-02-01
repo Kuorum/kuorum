9400a9cecbdb5b501420fb9ca46093dca1d7403e
Iñaki <iduetxe@gmail.com>
2015-02-01 18:43:17 +0100

    Faltaba renombrar un campo en la BBDD

--- grails-app/databaseMigration/scripts_1.3.6/1_refactorLaw.js
+++ grails-app/databaseMigration/scripts_1.3.6/1_refactorLaw.js
@@ -10,6 +10,7 @@ dbDest.kuorumFile.update({fileGroup: 'LAW_IMAGE'},{$set:{fileGroup:'PROJECT_IMAG
 dbDest.kuorumUser.update({"avatar.fileGroup":"LAW_IMAGE"},{$set:{"avatar.fileGroup":"PROJECT_IMAGE"}},{multi:true} );
 dbDest.kuorumUser.update({"imageProfile.fileGroup":"LAW_IMAGE"},{$set:{"imageProfile.fileGroup":"PROJECT_IMAGE"}},{multi:true} );
 dbDest.post.update({},{$rename:{'law':'project'}}, {multi:true});
+dbDest.projectVote.update({},{$rename:{'law':'project'}}, {multi:true});
 dbDest.post.update({"multimedia.fileGroup":"LAW_IMAGE"},{$set:{"multimedia.fileGroup":"PROJECT_IMAGE"}},{multi:1} );
 dbDest.project.update({"image.fileGroup":"LAW_IMAGE"},{$set:{"image.fileGroup":"PROJECT_IMAGE"}},{multi:1} );
 
