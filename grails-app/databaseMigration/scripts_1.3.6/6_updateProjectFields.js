7f379de496c6ae9b298fed6209c67db0ca6db7ba
Iñaki <iduetxe@gmail.com>
2015-02-15 11:49:37 +0100

    Actualizar nombre original de las imagenes embebidas del proyecto

--- grails-app/databaseMigration/scripts_1.3.6/6_updateProjectFields.js
+++ grails-app/databaseMigration/scripts_1.3.6/6_updateProjectFields.js
@@ -4,5 +4,14 @@ var dbDest = dbDest || connect("localhost:27017/Kuorum");
 dbDest.project.find().forEach(function (project){
     var numPost = dbDest.post.count({project:project._id, published:true})
     var description = project.introduction + "\r\n"+project.description
-    dbDest.project.update({_id:project._id},{$set:{'peopleVotes.numPosts':numPost, description:description}})
+    var imageName = project.image.fileName
+    dbDest.project.update(
+        {_id:project._id},
+        {$set:{
+            'peopleVotes.numPosts':numPost,
+            description:description,
+            'image.originalName':imageName
+            }
+        }
+    )
 })
