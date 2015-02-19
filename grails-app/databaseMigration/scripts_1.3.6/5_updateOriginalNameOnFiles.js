10afe57969444463bb5a4a449cf0d813f23774fe
iduetxe <iduetxe@gmail.com>
2015-02-19 11:20:41 +0100

    Actualizar el originalName en los avatares de los usuarios

--- grails-app/databaseMigration/scripts_1.3.6/5_updateOriginalNameOnFiles.js
+++ grails-app/databaseMigration/scripts_1.3.6/5_updateOriginalNameOnFiles.js
@@ -2,3 +2,13 @@
 var dbDest = dbDest || connect("localhost:27017/Kuorum");
 
 dbDest.kuorumFile.update({},{$set:{originalName:'XX'}},{multi:true});
+
+dbDest.kuorumUser.find().forEach(function (user){
+    if (user.avatar != undefined){
+        dbDest.kuorumUser.update({_id:user._id},{$set:{'avatar.originalName':'XX'}})
+    }
+
+    if (user.imageProfile != undefined){
+        dbDest.kuorumUser.update({_id:user._id},{$set:{'avatar.imageProfile':'XX'}})
+    }
+})
