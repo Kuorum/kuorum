18b102be93d8e3723a0ba077afaa46c4a376e998
matnso <matias.nso@kuorum.org>
2015-03-03 15:22:23 +0100

    proporción imágenes.

--- src/groovy/kuorum/core/FileGroup.groovy
+++ src/groovy/kuorum/core/FileGroup.groovy
@@ -5,9 +5,9 @@ package kuorum.core
 public enum FileGroup {
 
     USER_AVATAR("UsersFiles", 1024 * 1000 *8 /*1 MB */, '1', 110),
-    USER_PROFILE("UsersFiles", 1024 * 1000 *8/*1 MB */, '287/100', 110),
-    PROJECT_IMAGE("ProjectsFiles", 1024 * 1000 *10 /*10 MB */, '123/62', 738),
-    POST_IMAGE("PoliticianFiles", 1024 *1000 *8 /*8 MB */,'123/62',738),
+    USER_PROFILE("UsersFiles", 1024 * 1000 *8/*1 MB */, '16/9', 110),
+    PROJECT_IMAGE("ProjectsFiles", 1024 * 1000 *10 /*10 MB */, '16/9', 738),
+    POST_IMAGE("PoliticianFiles", 1024 *1000 *8 /*8 MB */,'16/9',738),
     PDF("PDFFiles", 1024 * 1000 *10 /*10 MB */,'',0),
     //TODO: Revisar el campo 'folderPath' para este caso
     YOUTUBE("YoutubeFiles",0,'',0)
