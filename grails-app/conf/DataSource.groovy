144eded4e3eb71d53ab4c2fa2d0ffa005c287712
Iñaki <iduetxe@gmail.com>
2015-03-11 20:55:08 +0100

    Datasource en el puerto correcto de local

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -20,7 +20,7 @@ environments {
         grails{
             mongo {
                 host = "localhost"
-                port = "27018"
+                port = "27017"
                 username = ""
                 password = ""
                 databaseName = "KuorumDev"
