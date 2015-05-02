86cc02b079b899dd394a74c4c3772ca2d0145dd5
Iñaki <iduetxe@gmail.com>
2015-05-02 18:58:33 +0200

    Estaba mal el código de UK

--- grails-app/databaseMigration/scripts_2.0.10/3_updateRegionPostalCodeHandler.js
+++ grails-app/databaseMigration/scripts_2.0.10/3_updateRegionPostalCodeHandler.js
@@ -3,6 +3,6 @@ var dbDest = dbDest || connect("localhost:27017/Kuorum");
 
 dbDest.region.update({"regionType" : "NATION"},{$set:{postalCodeHandlerType:'STANDARD_FIVE_DIGITS'}}, {multi:true});
 
-dbDest.region.update({"iso3166_2" : "UE-UK"},{$set:{postalCodeHandlerType:'UK'}} );
+dbDest.region.update({"iso3166_2" : "EU-UK"},{$set:{postalCodeHandlerType:'UK'}} );
 dbDest.region.update({"iso3166_2" : "LA-CO"},{$set:{postalCodeHandlerType:'STANDARD_SIX_DIGITS'}} );
 
