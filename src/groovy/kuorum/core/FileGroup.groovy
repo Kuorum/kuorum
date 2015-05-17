1389802969e4a8a13e55d56f22b5c0c09e34cceb
Iñaki <iduetxe@gmail.com>
2015-05-17 18:12:53 +0200

    Se ha reducido el tamaño al que se puede subir la imagen

--- src/groovy/kuorum/core/FileGroup.groovy
+++ src/groovy/kuorum/core/FileGroup.groovy
@@ -4,10 +4,10 @@ package kuorum.core
  */
 public enum FileGroup {
 
-    USER_AVATAR("UsersFiles", 1024 * 1000 *8 /*1 MB */, '1', 110),
-    USER_PROFILE("UsersFiles", 1024 * 1000 *8/*1 MB */, '16/9', 110),
-    PROJECT_IMAGE("ProjectsFiles", 1024 * 1000 *10 /*10 MB */, '16/9', 738),
-    POST_IMAGE("PoliticianFiles", 1024 *1000 *8 /*8 MB */,'16/9',738),
+    USER_AVATAR("UsersFiles", 1024 * 1000 *1 /*1 MB */, '1', 110),
+    USER_PROFILE("UsersFiles", 1024 * 1000 *1/*1 MB */, '16/9', 110),
+    PROJECT_IMAGE("ProjectsFiles", 1024 * 1000 *2 /*1 MB */, '16/9', 738),
+    POST_IMAGE("PoliticianFiles", 1024 *1000 *1 /*1 MB */,'16/9',738),
     PDF("PDFFiles", 1024 * 1000 *10 /*10 MB */,'',0),
     //TODO: Revisar el campo 'folderPath' para este caso
     YOUTUBE("YoutubeFiles",0,'',0)
