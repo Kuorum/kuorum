d6bb452d13dffe9c77c38018ceb137617c11aea1
iduetxe <iduetxe@gmail.com>
2015-05-18 13:17:44 +0200

    ALgunos alias

--- grails-app/databaseMigration/scripts_2.0.5/createAlias.js
+++ grails-app/databaseMigration/scripts_2.0.5/createAlias.js
@@ -13,3 +13,34 @@ dbDest.kuorumUser.update(
 
 
 dbDest.kuorumUser.createIndex( { alias: 1 } )
+
+
+dbDest.kuorumUser.update(
+    {_id:ObjectId("552be1b0e4b0e91aeaa5c733")},
+    {$set:{alias:"isabelrguez"}}
+)
+
+dbDest.kuorumUser.update(
+    {_id:ObjectId("552be1b0e4b0e91aeaa5c733")},
+    {$set:{email:"isabel.rodriguez@congreso.es"}}
+)
+
+dbDest.kuorumUser.update(
+    {_id:ObjectId("551957f5e4b0542cbfbf805a")},
+    {$set:{alias:"raul_salinero"}}
+)
+
+dbDest.kuorumUser.update(
+    {_id:ObjectId("551957f5e4b0542cbfbf805a")},
+    {$set:{email:"raul.salinero@iuburgos.es "}}
+)
+
+dbDest.kuorumUser.update({email:"federico.buyolo@congreso.es"},{$set:{alias:"fbuyolo"}})
+dbDest.kuorumUser.update({email:"carles.campuzano@gmail.com"},{$set:{alias:"carlescampuzano"}})
+dbDest.kuorumUser.update({email:"nachosamor@congreso.es"},{$set:{alias:"nachosamor"}})
+dbDest.kuorumUser.update({email:"ricardo.sixto@congreso.es"},{$set:{alias:"rsixtoiglesias"}})
+dbDest.kuorumUser.update({email:"patricia.hernandez@congreso.es"},{$set:{alias:"patriciahdezgut"}})
+dbDest.kuorumUser.update({email:"pablo.martin@congreso.es"},{$set:{alias:"pablomartin666"}})
+dbDest.kuorumUser.update({email:"gelorriaga@congreso.es"},{$set:{alias:"gabrielorriaga"}})
+dbDest.kuorumUser.update({email:"rafa.larreina@gmail.com"},{$set:{alias:"larreina"}})
+dbDest.kuorumUser.update({email:"elenabiurrun@telefonica.net"},{$set:{alias:"biurrun74"}})
