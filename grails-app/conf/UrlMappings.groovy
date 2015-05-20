2f3735aacc42171ada5191a9402e04544f147b0b
Iñaki <iduetxe@gmail.com>
2015-05-20 22:23:55 +0200

    URLMappings erroneo

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -13,7 +13,7 @@ class UrlMappings {
         //The URLs are duplicated because of the refactor of Law into Project. The name of aliases are changed.
         name home:             "/" (controller: "dashboard", action:"index")
         name dashboardSeeMore: "/ajax/dashboard/ver-mas" (controller: "dashboard", action:"dashboardClucks")
-        name discover:                      "/descubre" (controller: "dashboard", action:"discoverProjects")
+        name discover:                      "/descubre" (controller: "discover", action:"discoverProjects")
         name discoverProjects:              "/descubre/proyectos"         (controller: "discover", action:"discoverProjects")
                                             "/descubre/leyes"         (controller: "discover", action:"discoverProjects")
         name discoverProjectsByRegion:      "/descubre/proyectos/$regionName" (controller: "discover", action:"discoverProjects")
