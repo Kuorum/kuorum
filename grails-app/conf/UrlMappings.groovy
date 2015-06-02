1ef8dec1b67485f97eb0e2173b572fbdacebdd8c
iduetxe <iduetxe@gmail.com>
2015-06-02 12:22:26 +0200

    Nueva URL de ñapa para #losDatosCuentan

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -9,6 +9,7 @@ class UrlMappings {
         //TODO Hacer esta �apa en BBDD
 //        name tempmoralLink: "/$customLink" (controller: "dashboard", action:"customPostMapping")
         name tempmoralLink: "/losdatoscuentan" (controller: "dashboard", action:"customPostMapping")
+        name tempmoralLink: "/losDatosCuentan" (controller: "dashboard", action:"customPostMapping")
 
         //The URLs are duplicated because of the refactor of Law into Project. The name of aliases are changed.
         name home:             "/" (controller: "dashboard", action:"index")
