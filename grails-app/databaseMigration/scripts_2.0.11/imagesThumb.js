baec3e6cc46663716209a1bec109923c5e135fd4
Iñaki <iduetxe@gmail.com>
2015-07-19 19:02:59 +0200

    estaba mal el script

--- grails-app/databaseMigration/scripts_2.0.11/imagesThumb.js
+++ grails-app/databaseMigration/scripts_2.0.11/imagesThumb.js
@@ -1,7 +1,7 @@
 
 var dbDest = dbDest || connect("localhost:27017/Kuorum");
 
-dbDest.kuorumFile.find({url:/^http:/}).forEach(function(file) {
+dbDest.kuorumFile.find({}).forEach(function(file) {
     dbDest.kuorumFile.update({_id:file._id},{$set:{'urlThumb':file.url}})
 })
 
