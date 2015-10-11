bb4c2c3d644dff331fc0d0e3bf1fb5380e8be39c
Iñaki <iduetxe@gmail.com>
2015-10-11 20:42:44 +0200

    Indexar nuevo campo para los tags de los usuarios

--- src/groovy/kuorum/core/model/solr/SolrElement.groovy
+++ src/groovy/kuorum/core/model/solr/SolrElement.groovy
@@ -16,6 +16,7 @@ class SolrElement {
     String regionIso3166_2
     String institutionName //Project, Post and Politicians (Normal user and Organizations will be null)
     List<CommissionType> commissions
+    List<String> tags
     String urlImage
 
     //Not used for indexig. Is only for recovering data. On indexation time is skipped
