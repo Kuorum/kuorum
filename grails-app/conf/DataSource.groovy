eb7197c77065178a5530b2388bd617eb82b8a650
Iñaki <iduetxe@gmail.com>
2015-10-31 03:06:13 +0100

    Eliminada conexion salenda

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -50,19 +50,6 @@ environments {
         }
     }
 
-    salenda{
-        grails{
-            mongo {
-                host = "localhost"
-                port = "27017"
-                username = ""
-                password = ""
-                databaseName = "KuorumDev"
-            }
-        }
-
-    }
-
     production {
         grails{
             mongo {
