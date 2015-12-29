b30bc874a9e6be00f193cb179774bfd0acc66b2a
Iñaki <iduetxe@gmail.com>
2015-12-29 11:13:51 +0100

    Fix mongo script. Some users not have all fields

--- grails-app/databaseMigration/scripts_3.0.2/1_movePoliticianFields.js
+++ grails-app/databaseMigration/scripts_3.0.2/1_movePoliticianFields.js
@@ -3,22 +3,33 @@ var dbDest = dbDest || connect("localhost:27017/Kuorum");
 
 dbDest.kuorumUser.find({userType:'POLITICIAN'}).forEach(function(politician) {
 
-    dbDest.kuorumUser.update({_id:politician._id},{
-        $set:{
-            'careerDetails.university':politician.politicianExtraInfo.university,
-            'careerDetails.studies':politician.politicianExtraInfo.studies,
-            'careerDetails.school':politician.politicianExtraInfo.school,
-            'careerDetails.profession':politician.professionalDetails.profession,
-            'careerDetails.cvLink':politician.professionalDetails.cvLink,
-            'careerDetails.declarationLink':politician.professionalDetails.declarationLink
-        },
-        $unset:{
-            'politicianExtraInfo.university':'',
-            'politicianExtraInfo.studies':'',
-            'politicianExtraInfo.school':'',
-            'professionalDetails.profession':'',
-            'professionalDetails.cvLink':'',
-            'professionalDetails.declarationLink':''
-        }
-    })
+    if (politician.politicianExtraInfo != undefined){
+        dbDest.kuorumUser.update({_id:politician._id},{
+            $set:{
+                'careerDetails.university':politician.politicianExtraInfo.university,
+                'careerDetails.studies':politician.politicianExtraInfo.studies,
+                'careerDetails.school':politician.politicianExtraInfo.school
+            },
+            $unset:{
+                'politicianExtraInfo.university':'',
+                'politicianExtraInfo.studies':'',
+                'politicianExtraInfo.school':''
+            }
+        })
+    }
+
+    if (politician.professionalDetails != undefined){
+        dbDest.kuorumUser.update({_id:politician._id},{
+            $set:{
+                'careerDetails.profession':politician.professionalDetails.profession,
+                'careerDetails.cvLink':politician.professionalDetails.cvLink,
+                'careerDetails.declarationLink':politician.professionalDetails.declarationLink
+            },
+            $unset:{
+                'professionalDetails.profession':'',
+                'professionalDetails.cvLink':'',
+                'professionalDetails.declarationLink':''
+            }
+        })
+    }
 })
