3868426b5189a5550d1b2728d94ca43557490627
Iñaki <iduetxe@gmail.com>
2016-02-10 13:04:34 +0100

    Create DDBB Causes

---
+++ grails-app/databaseMigration/scripts_3.0.10/1_createCauses.js
@@ -0,0 +1,28 @@
+
+
+var dbDest = dbDest || connect("localhost:27017/Kuorum");
+
+
+
+dbDest.kuorumUser.aggregate([
+        {$match:{tags:{$exists:1}}},
+        {$unwind:"$tags"},
+        {$group:{_id:{tag:"$tags"}, count:{$sum:1}}}
+    ]).forEach(function(rawCause) {
+
+    var orgCauseName =rawCause._id.tag
+    var causeName =orgCauseName
+    if (causeName.substring(0, 1) == "#"){
+        causeName = orgCauseName.substring(1)
+        print("Chnging  "+orgCauseName +" to "+causeName)
+        dbDest.kuorumUser.update({tags:orgCauseName},{$pull:{tags:orgCauseName}},{multi:true})
+        dbDest.kuorumUser.update({tags:orgCauseName},{$addToSet:{tags:causeName}},{multi:true})
+    }
+
+    var cause = {
+        name: causeName,
+        citizenVotes: 0,
+        politicianVotes : rawCause.count
+    }
+    dbDest.cause.save(cause);
+})
