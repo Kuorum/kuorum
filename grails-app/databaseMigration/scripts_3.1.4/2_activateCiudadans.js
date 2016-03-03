69458d0e12ade6f2c5b3d81de7e4e52e04cd9ad0
Iñaki <iduetxe@gmail.com>
2016-03-03 20:09:19 +0100

    Roles wrong recovering

--- grails-app/databaseMigration/scripts_3.1.4/2_activateCiudadans.js
+++ grails-app/databaseMigration/scripts_3.1.4/2_activateCiudadans.js
@@ -1,16 +1,16 @@
 var dbDest = dbDest || connect("localhost:27017/Kuorum");
 
-var basicRole = dbDest.roleUser.find({authority:"ROLE_USER"})
-var politicianRole = dbDest.roleUser.find({authority:"ROLE_POLITICIAN"})
+var basicRole = dbDest.roleUser.find({authority:"ROLE_USER"}).next()
+var politicianRole = dbDest.roleUser.find({authority:"ROLE_POLITICIAN"}).next()
 
 dbDest.kuorumUser.update(
     {'professionalDetails.politicalParty':'Ciudadanos'},
     {
         $set:{
             enabled:true,
-            "password" : "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"
-        },
-        $addToSet:{authorities:[basicRole, politicianRole]}
+            "password" : "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08",
+            authorities:[basicRole, politicianRole]
+        }
     },
     {multi:true}
 )
