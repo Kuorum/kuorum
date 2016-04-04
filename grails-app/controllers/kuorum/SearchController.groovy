7fed595af2736aa2febad00c8a6cbc801ca51b6f
Iñaki <iduetxe@gmail.com>
2016-04-04 14:41:59 +0200

    Fix Search params

--- grails-app/controllers/kuorum/SearchController.groovy
+++ grails-app/controllers/kuorum/SearchController.groovy
@@ -128,6 +128,7 @@ class SearchController{
             }
             searchParams.max = max
             docs = searchSolrService.search(searchParams)
+            searchParams.word = params.word
         }
         return docs;
     }
