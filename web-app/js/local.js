e7218aedc6875f9593629be8e124d63bd9a6796a
Iñaki <iduetxe@gmail.com>
2016-04-27 17:51:37 +0200

    On non profile page, the chart fails because not exists its div

--- web-app/js/local.js
+++ web-app/js/local.js
@@ -121,159 +121,161 @@ $(function () {
 
 
 function printChart(){
-    $('#polValChart').html("")
-    Highcharts.setOptions({
-        colors: ['#ff9431', '#999999', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4'],
-        global: {
-            useUTC: false
-        }
-    });
-    var urlHighchart=$('#polValChart').attr("data-urljs")
-    if (urlHighchart == undefined){
-        urlHighchart = 'mock//valpol.json'
-    }
-
-    $.getJSON(urlHighchart, function (activity) {
-        var seriesData = []
-        $.each(activity.datasets, function (i, dataset) {
-
-            seriesData[i]={
-                data: dataset.data,
-                name: dataset.name,
-                type: dataset.type,
-                pointInterval: 1 * 3600 * 1000, // cada 1h
-                color: Highcharts.getOptions().colors[i],
-                fillOpacity: 0.3,
-                tooltip: {
-                    valueSuffix: ' ' + dataset.unit
-                }
+    if ($('#polValChart').length >0){
+        $('#polValChart').html("")
+        Highcharts.setOptions({
+            colors: ['#ff9431', '#999999', '#ED561B', '#DDDF00', '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4'],
+            global: {
+                useUTC: false
             }
         });
-        $('<div class="chart">')
-            .appendTo('#polValChart')
-            .highcharts('StockChart', {
-                chart: {
-                    spacingBottom: 10,
-                    spacingTop: -23,
-                    zoomType: 'x',
-                    height: 300,
-                    events : {
-                        load : function () {
-                            // set up the updating of the chart each second
-                            var series = this.series;
-                            setInterval(function () {
-                                $.getJSON(urlHighchart, function (activity) {
-                                    $.each(activity.datasets, function (i, dataset) {
-                                        series[i].setData(dataset.data);
-                                    });
-                                });
+        var urlHighchart=$('#polValChart').attr("data-urljs")
+        if (urlHighchart == undefined){
+            urlHighchart = 'mock//valpol.json'
+        }
 
-                            }, 1000);
-                        }
+        $.getJSON(urlHighchart, function (activity) {
+            var seriesData = []
+            $.each(activity.datasets, function (i, dataset) {
+
+                seriesData[i]={
+                    data: dataset.data,
+                    name: dataset.name,
+                    type: dataset.type,
+                    pointInterval: 1 * 3600 * 1000, // cada 1h
+                    color: Highcharts.getOptions().colors[i],
+                    fillOpacity: 0.3,
+                    tooltip: {
+                        valueSuffix: ' ' + dataset.unit
                     }
-                },
-                title: {
-                    text: null,
-                    //text: activity.title,
-                    align: 'left',
-                    margin: 5,
-                    color: '#666666',
-                    x: 0
-                },
-                credits: {
-                    enabled: false
-                },
-                legend: {
-                    enabled: true,
-                    layout:"horizontal",
-                    verticalAlign:"top",
-                    align:"left",
-                    floating:true,
-                    itemStyle:
-                    {
-                        fontSize:'14px',
-                        fontWeight:'normal'
-                    },
-                    y:15,
-                    x:0
-                },
-                xAxis: {
-                    type: 'datetime',
-                    //minTickInterval: 24 * 1000 * 3600, // intervalo cada 1h
-                    //minorTickInterval: 24 * 1000 * 3600, // intervalo cada 1h
-                    range: 3 * 24 * 3600 * 1000, // mostramos 1 semana
-                    dateTimeLabelFormats:{
-                        millisecond: '%H:%M:%S.%L',
-                        second: '%H:%M:%S',
-                        minute: '%H:%M',
-                        hour: '%H:%M',
-                        day: '%e/%m',
-                        week: '%e/%m',
-                        month: '%b \'%y',
-                        year: '%Y'
-                    }
-                },
-                yAxis: {
-                    title: {
-                        text: null
-                    },
-                    allowDecimals: false,
-                    crosshair: true,
-                    //minTickInterval: 1,
-                    minorGridLineColor: '#F0F0F0',
-                    //minorTickInterval:null,
-                    tickPositions: [1, 2, 3, 4, 5, 6],
-                    plotLines: [{
-                        value: activity.average,
-                        width: 1,
-                        color: '#666666',
-                        dashStyle: 'dash',
-                        label: {
-                            text: activity.averageLabel,
-                            align: 'left',
-                            y: 0,
-                            x: -2,
-                            rotation:270,
-                            style:{
-                                color:'#666666'
+                }
+            });
+            $('<div class="chart">')
+                .appendTo('#polValChart')
+                .highcharts('StockChart', {
+                    chart: {
+                        spacingBottom: 10,
+                        spacingTop: -23,
+                        zoomType: 'x',
+                        height: 300,
+                        events : {
+                            load : function () {
+                                // set up the updating of the chart each second
+                                var series = this.series;
+                                setInterval(function () {
+                                    $.getJSON(urlHighchart, function (activity) {
+                                        $.each(activity.datasets, function (i, dataset) {
+                                            series[i].setData(dataset.data);
+                                        });
+                                    });
+
+                                }, 1000);
                             }
                         }
-                    }],
-                    offset: 20
-                },
-                tooltip: {
-                    positioner: function () {
-                        return {
-                            x: 5,
-                            y: -11
-                        };
                     },
-                    backgroundColor: 'rgba(255, 255, 255, 1)',
-                    borderWidth: 0,
-                    pointFormat: '<span style="color:{point.color}"> {series.name}: <b>{point.y}</b></span>',
-                    //pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
-                    headerFormat: '',
-                    shadow: false,
-                    style: {
-                        fontSize: '15px'
+                    title: {
+                        text: null,
+                        //text: activity.title,
+                        align: 'left',
+                        margin: 5,
+                        color: '#666666',
+                        x: 0
                     },
-                    valueDecimals: 2,
-                },
-                rangeSelector: {
-                    enabled: false
-                },
-                scrollbar: {
-                    enabled: false
-                },
-                navigator: {
-                    height: 20,
-                    margin: 10,
-                    maskFill: 'rgba(0, 0, 0, 0.05)',
-                    maskInside: false
-                },
-                series: seriesData
-            });
-    });
+                    credits: {
+                        enabled: false
+                    },
+                    legend: {
+                        enabled: true,
+                        layout:"horizontal",
+                        verticalAlign:"top",
+                        align:"left",
+                        floating:true,
+                        itemStyle:
+                        {
+                            fontSize:'14px',
+                            fontWeight:'normal'
+                        },
+                        y:15,
+                        x:0
+                    },
+                    xAxis: {
+                        type: 'datetime',
+                        //minTickInterval: 24 * 1000 * 3600, // intervalo cada 1h
+                        //minorTickInterval: 24 * 1000 * 3600, // intervalo cada 1h
+                        range: 3 * 24 * 3600 * 1000, // mostramos 1 semana
+                        dateTimeLabelFormats:{
+                            millisecond: '%H:%M:%S.%L',
+                            second: '%H:%M:%S',
+                            minute: '%H:%M',
+                            hour: '%H:%M',
+                            day: '%e/%m',
+                            week: '%e/%m',
+                            month: '%b \'%y',
+                            year: '%Y'
+                        }
+                    },
+                    yAxis: {
+                        title: {
+                            text: null
+                        },
+                        allowDecimals: false,
+                        crosshair: true,
+                        //minTickInterval: 1,
+                        minorGridLineColor: '#F0F0F0',
+                        //minorTickInterval:null,
+                        tickPositions: [1, 2, 3, 4, 5, 6],
+                        plotLines: [{
+                            value: activity.average,
+                            width: 1,
+                            color: '#666666',
+                            dashStyle: 'dash',
+                            label: {
+                                text: activity.averageLabel,
+                                align: 'left',
+                                y: 0,
+                                x: -2,
+                                rotation:270,
+                                style:{
+                                    color:'#666666'
+                                }
+                            }
+                        }],
+                        offset: 20
+                    },
+                    tooltip: {
+                        positioner: function () {
+                            return {
+                                x: 5,
+                                y: -11
+                            };
+                        },
+                        backgroundColor: 'rgba(255, 255, 255, 1)',
+                        borderWidth: 0,
+                        pointFormat: '<span style="color:{point.color}"> {series.name}: <b>{point.y}</b></span>',
+                        //pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+                        headerFormat: '',
+                        shadow: false,
+                        style: {
+                            fontSize: '15px'
+                        },
+                        valueDecimals: 2,
+                    },
+                    rangeSelector: {
+                        enabled: false
+                    },
+                    scrollbar: {
+                        enabled: false
+                    },
+                    navigator: {
+                        height: 20,
+                        margin: 10,
+                        maskFill: 'rgba(0, 0, 0, 0.05)',
+                        maskInside: false
+                    },
+                    series: seriesData
+                });
+        });
+    }
 }
 
 $(document).ready(function() {
