337d4faee2046f90b9d504b56c245f1f07a6377f
Iñaki <iduetxe@gmail.com>
2016-12-20 14:25:50 +0100

    KPV-379 =>  Scritp for split name

--- grails-app/databaseMigration/scripts_v4/scripts_4.0.9/1_splitNameAndSurname.js
+++ grails-app/databaseMigration/scripts_v4/scripts_4.0.9/1_splitNameAndSurname.js
@@ -1,8 +1,12 @@
 var dbDest = dbDest || connect("localhost:27017/Kuorum");
 
 dbDest.kuorumUser.find({}).forEach(function(user) {
-    var fullName = user.name;
-    var name = fullName.substr(0,fullName.indexOf(' '));
-    var surname = fullName.substr(fullName.indexOf(' ')+1); //
+    var fullName = user.name.trim();
+    var name = fullName;
+    var surname = "";
+    if (fullName.indexOf(' ')>0){
+        name = fullName.substr(0,fullName.indexOf(' '));
+        surname = fullName.substr(fullName.indexOf(' ')+1); //
+    }
     dbDest.kuorumUser.update({_id:user._id},{$set:{'name':name, 'surname':surname}})
 })
