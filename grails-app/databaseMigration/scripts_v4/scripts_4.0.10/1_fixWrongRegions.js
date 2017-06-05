ad8d8eb42bf4e47711848407e9780ebe15672dff
Iñaki <iduetxe@gmail.com>
2017-06-05 12:02:27 +0200

    KPV-709 :: Clean wrong regions

--- grails-app/databaseMigration/scripts_v4/scripts_4.0.10/1_fixWrongRegions.js
+++ grails-app/databaseMigration/scripts_v4/scripts_4.0.10/1_fixWrongRegions.js
@@ -2,20 +2,26 @@ var dbDest = dbDest || connect("localhost:27017/Kuorum");
 
 
 var count = 0;
-dbDest.kuorumUser.find({}).forEach(function(user) {
+dbDest.kuorumUser.find().forEach(function(user) {
 
     if (user.personalData != undefined){
         if (user.personalData.province !=undefined){
-            var existsRegion = dbDest.kuorumRegion.find({_id:user.personalData.province}).hasNext();
+            var existsRegion = dbDest.region.find({_id:user.personalData.province}).hasNext();
             if (!existsRegion){
                 print("Has no province ");
                 dbDest.kuorumUser.update({_id:user._id},{$unset:{'personalData.province':''}});
+                if (dbDest.region.find({iso3166_2:user.personalData.provinceCode}).hasNext()){
+                    var newRegion = dbDest.region.find({iso3166_2:user.personalData.provinceCode}).next()
+                    print("Updating province "+newRegion.name);
+                    dbDest.kuorumUser.update({_id:user._id},{$unset:{'personalData.province':newRegion._id}});
+                }
+
             }
             count ++;
         }
 
         if (user.personalData.country != undefined){
-            var existsRegion = dbDest.kuorumRegion.find({_id:user.personalData.country}).hasNext();
+            var existsRegion = dbDest.region.find({_id:user.personalData.country}).hasNext();
             if (!existsRegion){
                 print("Has no Country ");
                 dbDest.kuorumUser.update({_id:user._id},{$unset:{'personalData.country':''}})
