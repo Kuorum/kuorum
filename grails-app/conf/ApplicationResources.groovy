3db475c6445dbf865471e813b9e9dfdbe516a01e
Iñaki <inaki.dominguez@kuorum.org>
2017-09-08 10:08:09 +0200

    KPV-812 :: Add js dependency

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -93,7 +93,7 @@ modules = {
     }
 
     debate {
-        dependsOn("basic", "forms")
+        dependsOn("basic", "forms", 'kuorumCookies')
         resource url:'js/medium-editor/medium-editor.js'
         resource url:'css/medium-editor/medium-editor.min.css'
         resource url:'css/medium-editor/themes/default.css'
