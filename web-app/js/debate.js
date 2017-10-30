86f4b97b9d595169f97ef443d91b5bb969b338e8
iduetxe <36a99c9b2>
2017-10-30 12:22:56 +0100

    KPV-879 :: Fix call to action on not published debate

--- web-app/js/debate.js
+++ web-app/js/debate.js
@@ -196,20 +196,26 @@ $(function () {
     var $callToAction = $('body').find('.comment-box.call-to-action');
     $callToAction.find('.comment.editable').focus();
 
-    $(window).scroll(function () {
-        var upperLimit = $("section#main .comment-box").offset();
-        var buttonPosition = $("section#main .leader-post .header .call-to-action-mobile").offset();
-        if (buttonPosition.top > upperLimit.top) {
-            if ($(".call-to-action-mobile").is(":visible")) {
-                $('.call-to-action-mobile').toggleClass('hidden');
+    if($("section#main .comment-box").length > 0){
+        // Debate not published has no comment-boxes
+        $(window).scroll(function () {
+            var upperLimit = $("section#main .comment-box").offset();
+            var buttonPosition = $("section#main .leader-post .header .call-to-action-mobile").offset();
+            if (buttonPosition.top > upperLimit.top) {
+                if ($(".call-to-action-mobile").is(":visible")) {
+                    $('.call-to-action-mobile').toggleClass('hidden');
+                }
             }
-        }
-        else if($(".call-to-action-mobile").hasClass("hidden")){
-            if((buttonPosition.top + 150) < upperLimit.top){
-                $('.call-to-action-mobile').toggleClass('hidden');
+            else if($(".call-to-action-mobile").hasClass("hidden")){
+                if((buttonPosition.top + 150) < upperLimit.top){
+                    $('.call-to-action-mobile').toggleClass('hidden');
+                }
             }
-        }
-    });
+        });
+    }else{
+        // Debate not published => Call to action hide due to is not possible to add comments
+        $(".leader-post .call-to-action-mobile").hide()
+    }
 
     $(".call-to-action-mobile button").on("click", function(){
         var $proposalBox = $(".comment-box .comment.editable");
