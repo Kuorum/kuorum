192bf61bdd9bbbea83cfb2b7e60563719894ad9c
iduetxe <iduetxe@gmail.com>
2018-03-27 00:00:11 +0200

    KPV-967 :: Multidomain log with development url

--- src/groovy/kuorum/core/customDomain/filter/CustomDomainSpringFilter.groovy
+++ src/groovy/kuorum/core/customDomain/filter/CustomDomainSpringFilter.groovy
@@ -18,7 +18,8 @@ class CustomDomainSpringFilter extends GenericFilterBean {
         URL url = new URL(request.getRequestURL().toString());
         if (url.getHost() == "127.0.0.1"){
             // Debug on idea via apache using proxy always is 127.0.0.1
-            url = new URL("http://local2.kuorum.org/kuorum")
+            logger.warn("Develop mode. Using local.kuorum.org")
+            url = new URL("http://local.kuorum.org/kuorum")
         }
         CustomDomainResolver.setUrl(url, request.getContextPath())
         Map<String, String> params = [:]
