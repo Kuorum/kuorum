8da5f03afbe4b630a519ec5ed8e770e43ea0736b
iduetxe <iduetxe@gmail.com>
2018-06-06 14:43:34 +0200

    KPV-967 KPV-1021 :: Google login

--- web-app/js/social/loginApi.js
+++ web-app/js/social/loginApi.js
@@ -1,38 +1,43 @@
 
 $(function (){
-    $(".socialGo .btn.fb").on("click", function(e){
-        e.preventDefault();
+    var callbackError = function(msg){
+        console.log("Error login: "+msg)
+        display.warn(i18n.register.errors)
+    };
+    var apiCallback = function(response) {
+        pageLoadingOn("Api login")
         var callbackFunctionName = $('#registro').find("form").attr("callback");
         var callbackSuccess = noLoggedCallbacks[callbackFunctionName] || noLoggedCallbacks.reloadPage
-        var callbackError = function(msg){
-            console.log("Error login: "+msg)
-            display.warn(i18n.register.errors)
-        };
-
-        var apiCallback = function(response) {
-            pageLoadingOn("Api login")
-            if (response.success)
-            {
-                var data = response;
-                data['provider']='api';
-                $.ajax({
-                    type:"POST",
-                    url:kuorumUrls.ajaxLoginRRSS,
-                    data:data,
-                    dataType:"json"
-                }).success(function(data){
-                    callbackSuccess()
-                }).error(function(jqXHR, textStatus,errorThrown){
-                    callbackError()
-                    pageLoadingOn("Error Callback")
-                }).done(function(data){
-                });
-            }else{
+        if (response.success)
+        {
+            var data = response;
+            data['provider']='api';
+            $.ajax({
+                type:"POST",
+                url:kuorumUrls.ajaxLoginRRSS,
+                data:data,
+                dataType:"json"
+            }).success(function(data){
+                callbackSuccess()
+            }).error(function(jqXHR, textStatus,errorThrown){
                 callbackError()
                 pageLoadingOn("Error Callback")
-            }
+            }).done(function(data){
+            });
+        }else{
+            callbackError()
+            pageLoadingOn("Error Callback")
         }
+    }
+
+    $(".socialGo .btn.fb").on("click", function(e){
+        e.preventDefault();
         var socialButton = new SocialButton("facebook",apiCallback, callbackError);
+        socialButton.openSocialLoginWindow()
+    })
+    $(".socialGo .btn.gog").on("click", function(e){
+        e.preventDefault();
+        var socialButton = new SocialButton("google",apiCallback, callbackError);
         socialButton.openSocialLoginWindow()
     })
 })
