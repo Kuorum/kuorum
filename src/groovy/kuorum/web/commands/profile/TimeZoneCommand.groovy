7406eea743b69857cc1e72843a1ec2580d58c1c6
iduetxe <iduetxe@gmail.com>
2018-12-10 12:00:48 +0100

    Optimize imports

--- src/groovy/kuorum/web/commands/profile/TimeZoneCommand.groovy
+++ src/groovy/kuorum/web/commands/profile/TimeZoneCommand.groovy
@@ -1,13 +1,7 @@
 package kuorum.web.commands.profile
 
 import grails.validation.Validateable
-import kuorum.Region
-import kuorum.core.model.AvailableLanguage
 import kuorum.users.KuorumUser
-import kuorum.web.binder.RegionBinder
-import org.codehaus.groovy.grails.web.context.ServletContextHolder
-import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes
-import org.grails.databinding.BindUsing
 
 /**
  * Created by iduetxe on 4/01/16.
@@ -15,12 +9,13 @@ import org.grails.databinding.BindUsing
 @Validateable
 class TimeZoneCommand {
 
-    public TimeZoneCommand(){}
-    public TimeZoneCommand(KuorumUser user){
+    TimeZoneCommand(){}
+
+    TimeZoneCommand(KuorumUser user){
         this.timeZoneId = user.timeZone?.getID()
     }
 
-    String timeZoneRedirect;
+    String timeZoneRedirect
     String timeZoneId
 
 
