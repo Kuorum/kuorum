94d6cf5370ac16a01c6fa816460c8e4e39ae204d
iduetxe <iduetxe@gmail.com>
2019-09-24 11:50:12 +0200

    KPV-1305 :: Overwirte Font-Family with domain configuration (Removed Bold option)

--- src/groovy/kuorum/web/admin/domain/KuorumWebFont.groovy
+++ src/groovy/kuorum/web/admin/domain/KuorumWebFont.groovy
@@ -2,14 +2,15 @@ package kuorum.web.admin.domain
 
 enum KuorumWebFont {
 
+//    MONTSERRAT_MONTSERRATBOLD("Montserrat, sans-serif","Montserrat, sans-serif", "700"),
+//    ROBOTO_ROBOTOBOLD("Roboto, sans-serif","Roboto, sans-serif", "700"),
     HELVETICA_ARIAL("\"Helvetica Neue\", Helvetica,Arial,sans-serif","Arial" ),
-    ARIAL_ARIAL("Arial", "Arial", "bold"),
+    ARIAL_ARIAL("Arial", "Arial", ),
+//    ARIAL_ARIALBOLD("Arial", "Arial", "bold"),
     HELVETICA_GEORGIA("\"Helvetica Neue\", Helvetica,Arial,sans-serif", "Georgia"),
     UBUNTU_HELVETICA("Ubuntu, Helvetica","\"Helvetica Neue\", Helvetica, sans-serif"),
     MERRIWHEADER_OPENSANS("'Merriweather', serif","'Open Sans', sans-serif"),
-    LATO_MERRIWHEADER("'Lato', sans-serif","'Merriweather', serif"),
-    MONTSERRAT_MONTSERRATBOLD("Montserrat, sans-serif","Montserrat, sans-serif", "700"),
-    ROBOTO_ROBOTOBOLD("Roboto, sans-serif","Roboto, sans-serif", "700");
+    LATO_MERRIWHEADER("'Lato', sans-serif","'Merriweather', serif");
 
     String titleFontName;
     String titleFontWeight;
