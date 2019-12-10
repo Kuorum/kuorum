38bd252104c41de9e2c76d75ff287ab784371753
iduetxe <iduetxe@gmail.com>
2019-12-10 15:42:03 +0100

    KPV-1335 :: Contacts with phone (phone could be null)

--- src/groovy/kuorum/web/commands/payment/contact/ContactCommand.groovy
+++ src/groovy/kuorum/web/commands/payment/contact/ContactCommand.groovy
@@ -24,6 +24,7 @@ class ContactCommand {
         name nullable: false
         surname nullable: true
         language nullable: true
+        phone nullable: true
         email nullable: true, email: true // Is nullable because when the contact is a follower we don't have the email
     }
 }
