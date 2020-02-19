4d3e129afb3fb454823593a5fe9095c8b83ed945
iduetxe <iduetxe@gmail.com>
2020-02-19 15:31:51 +0100

    KPV-1369 :: Changed algorithm of the visibility of the "Call to action on mobile"

--- web-app/js/campaigns/campaign.js
+++ web-app/js/campaigns/campaign.js
@@ -7,15 +7,18 @@ var campaignFunctions={
         if($("section#main .comment-box").length > 0 && $("section#main .leader-post .header .call-to-action-mobile").length > 0){
             // Debate not published has no comment-boxes
             $(window).scroll(function () {
-                var upperLimit = $("section#main .comment-box").offset();
+                // var upperLimit = $("section#main .comment-box").offset();
+                var campaignMainInfo = $("section#main .leader-post");
+                var upperLimit = campaignMainInfo.offset().top + campaignMainInfo.height()
+                console.log(upperLimit)
                 var buttonPosition = $("section#main .leader-post .header .call-to-action-mobile").offset();
-                if (buttonPosition.top > upperLimit.top) {
+                if (buttonPosition.top > upperLimit) {
                     if ($(".call-to-action-mobile").is(":visible")) {
                         $('.call-to-action-mobile').toggleClass('hidden');
                     }
                 }
                 else if($(".call-to-action-mobile").hasClass("hidden")){
-                    if((buttonPosition.top + 150) < upperLimit.top){
+                    if((buttonPosition.top + 150) < upperLimit){
                         $('.call-to-action-mobile').toggleClass('hidden');
                     }
                 }
