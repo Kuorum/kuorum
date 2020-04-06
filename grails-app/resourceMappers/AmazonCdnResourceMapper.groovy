ef99a98d6a3deef0b8953affba6af13f71ed3b0d
iduetxe <iduetxe@gmail.com>
2020-04-06 02:59:39 +0200

    KPV-1424 :: CDN with amazon [Cloud front] - JNDI is uploaded on builging process

--- grails-app/resourceMappers/AmazonCdnResourceMapper.groovy
+++ grails-app/resourceMappers/AmazonCdnResourceMapper.groovy
@@ -15,17 +15,21 @@ class AmazonCdnResourceMapper {
 
     def map(resource, config) {
         if (config?.enabled){
+            String keyName = "${config.path}${resource.actualUrl}".replace("//","/").replaceAll(/^\//,"")
             try{
-
                 File file;
                 if (resource.originalResource != null){
                     file = ((org.springframework.core.io.UrlResource) resource.originalResource).file;
                 }else{
                     file = new File("${resource.workDir}${resource.actualUrl}");
                 };
-                String keyName = "${config.path}${resource.actualUrl}"
-                String path = uploadFileToAmazon(file, resource.contentType, keyName, config);
-                resource.linkOverride = "https://${config.host}/${path}".toString();
+                if (file.absolutePath.contains("bundle")){
+                    String path = uploadFileToAmazon(file, resource.contentType, keyName, config);
+                    resource.linkOverride = "https://${config.host}/${path}".toString();
+                }else{
+                    log.warn("File ${keyname} was uploaded on deploy process")
+                    resource.linkOverride = "https://${config.host}/${keyName}".toString();
+                }
             }catch(Exception e){
                 log.error("Error deploying resource on AMAZON : ${resource.linkOverride} :: ${e.getMessage()}")
             }
