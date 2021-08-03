9aec313766242a3448aea985fc9f1a81fa6991c2
albercv <alberto.carrasco@kuorum.org>
2021-08-03 09:16:46 +0200

    dev/KPV-1852 :: Fixed property hardcoded that changed always selected property changed to min whenever save draft

--- src/groovy/kuorum/web/commands/payment/survey/SurveyQuestionsCommand.groovy
+++ src/groovy/kuorum/web/commands/payment/survey/SurveyQuestionsCommand.groovy
@@ -143,10 +143,7 @@ enum QuestionLimitAnswersType{
     MIN,MAX,RANGE,FORCE
 
     public static QuestionLimitAnswersType inferType(QuestionRSDTO questionRSDTO){
-        if (questionRSDTO.questionType == QuestionTypeRSDTO.MULTIPLE_OPTION_WEIGHTED){
-            // Multiple option weight has no limit. The limit is the max calculated with his ponderation.
-            return QuestionLimitAnswersType.MIN;
-        }else if (questionRSDTO.maxAnswers == questionRSDTO.minAnswers){
+        if (questionRSDTO.maxAnswers == questionRSDTO.minAnswers){
             return QuestionLimitAnswersType.FORCE
         }else if (questionRSDTO.minAnswers != 1 && questionRSDTO.maxAnswers != 0 ){
             return QuestionLimitAnswersType.RANGE
