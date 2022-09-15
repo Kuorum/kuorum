748a1d536ce7882d5128e380e3e9c4d88a9295ab
Pablo Verdú <pablo.verdu@kuorum.org>
2022-09-15 13:22:27 +0200

    KPV-2320 :: Fixed the contest application vote logic, now should disable proper buttons when the user is known or anonymous. Buttons are disabled if the contest application is already voted for the logged user.

--- web-app/js/campaigns/contestApplication.js
+++ web-app/js/campaigns/contestApplication.js
@@ -18,7 +18,7 @@ var contestApplicationFunctions = {
         event.stopPropagation();
         var $button = $(this);
 
-        if ($button.attr("disabled") == undefined) {
+        if ($button.attr("btn-disabled") === "false") {
             var params = {
                 callback: undefined,
                 $button: $button
@@ -62,14 +62,19 @@ var contestApplicationFunctions = {
                 if (contestApplicationVote.success) {
                     console.log("Vote Success")
                     console.log(contestApplicationVote)
-                    var $buttonMainPage = $("section#main .leader-post .contestApplication-vote")
-                    var $buttonColumnC = $("#aside-ppal .call-to-action .actions a")
-                    var $liStats = $(".leader-post-stats .fa-scroll").parents("li")
+                    const $buttonMainPage = $("section#main .leader-post .contestApplication-vote")
+                    const $buttonColumnC = $("#aside-ppal .call-to-action .actions .contestApplication-vote a")
+                    const $buttonContestApplicationList = $("#contest-applications-list  .contestApplication-vote-" + contestApplicationVote.vote.contestApplicationId)
+                    const $liStats = $(".leader-post-stats .fa-scroll").parents("li")
 
-                    contestApplicationFunctions._disableButton($button, contestApplicationVote.vote.votes);
-                    contestApplicationFunctions._disableButton($buttonMainPage, contestApplicationVote.vote.votes);
-                    contestApplicationFunctions._disableButton($buttonColumnC, contestApplicationVote.vote.votes);
-                    contestApplicationFunctions._disableButton($liStats, contestApplicationVote.vote.votes);
+                    if($button.attr("data-loggedUser") !== '') {
+                        contestApplicationFunctions._disableVoteButton($buttonMainPage, contestApplicationVote.vote.votes);
+                        contestApplicationFunctions._disableVoteButton($buttonColumnC, contestApplicationVote.vote.votes);
+                        contestApplicationFunctions._disableVoteButton($buttonContestApplicationList, contestApplicationVote.vote.votes);
+                    }
+                    contestApplicationFunctions._updateNumVotes($buttonMainPage, contestApplicationVote.vote.votes);
+                    contestApplicationFunctions._updateNumVotes($liStats, contestApplicationVote.vote.votes);
+                    contestApplicationFunctions._updateNumVotes($buttonContestApplicationList, contestApplicationVote.vote.votes);
                     display.success(contestApplicationVote.message)
                 } else {
                     display.error(contestApplicationVote.message)
@@ -84,12 +89,19 @@ var contestApplicationFunctions = {
         });
     },
 
-    _disableButton: function ($button, numVotes) {
-        $button.attr("disabled", "disabled")
+    _disableVoteButton: function ($button, numVotes) {
+        $button.attr("btn-disabled", "true")
         $button.addClass("active")
         $button.addClass("disabled")
         $button.find(".fa-scroll").removeClass("fal")
         $button.find(".fa-scroll").addClass("fas")
+
+        if ($button.parent().hasClass("actions")) {
+            $button.html($button.attr("data-disabledText"))
+        }
+    },
+
+    _updateNumVotes: function ($button, numVotes) {
         $button.find(".number").html(numVotes)
     }
 };
