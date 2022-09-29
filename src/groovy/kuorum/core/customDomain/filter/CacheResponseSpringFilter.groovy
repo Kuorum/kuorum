363ed9984104d6811c2feedc96c7158b4a26dbdb
Iñaki <inaki.dominguez@kuorum.org>
2022-09-29 11:08:45 +0200

    kpv-2506 :: Structura para meter una cache de HTML

---
+++ src/groovy/kuorum/core/customDomain/filter/CacheResponseSpringFilter.groovy
@@ -0,0 +1,64 @@
+package kuorum.core.customDomain.filter
+
+import grails.plugin.springsecurity.SpringSecurityService
+import kuorum.domain.DomainService
+import kuorum.register.KuorumUserSession
+import org.springframework.web.filter.GenericFilterBean
+
+import javax.servlet.FilterChain
+import javax.servlet.ServletException
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import javax.servlet.http.HttpServletResponse
+
+class CacheResponseSpringFilter extends GenericFilterBean {
+
+    DomainService domainService
+
+    SpringSecurityService springSecurityService
+
+    // TODO: CHANGE CACHE
+    HashMap<String, String> fakeCache = new HashMap<>();
+
+    void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        URL url = new URL(request.getRequestURL().toString())
+        String rawHTML = getCachedWeb(url);
+        if (rawHTML && isURLCacheable(URL)) {
+            // TODO: RECOVER DATA FROM CACHE AND FILL THE RESPONSE
+        } else {
+            filterChain.doFilter(request, response)
+            saveCache(response)
+        }
+    }
+
+    Boolean isURLCacheable(URL url) {
+        // TODO: THINK WHICH URLS SHOULD BE CACHED
+        return true
+    }
+
+    String getCachedWeb(URL url) {
+        String urlKey = buildKey(url)
+        if (fakeCache.containsKey(urlKey)) {
+            return fakeCache.get(urlKey);
+        } else {
+            return null;
+        }
+    }
+
+    String buildKey(URL url) {
+        if (springSecurityService.isLoggedIn()) {
+            return url.toString() + ((KuorumUserSession) springSecurityService.principal).id
+        } else {
+            return url.toString();
+        }
+    }
+
+    void saveCache(URL url, ServletResponse response) {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        if (isURLCacheable(url)) {
+            // TODO: RECOVER DATA FROM RESPONSE AND SAVE RESPONSE
+
+        }
+    }
+
+}
