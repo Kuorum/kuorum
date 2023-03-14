a92b240b81c6ce1b75b8d07084d28ba1794ecffc
Iñaki <inaki.dominguez@kuorum.org>
2023-03-14 14:23:21 +0100

    Phone Validation

--- src/groovy/kuorum/web/commands/profile/DomainUserPhoneValidationCommand.groovy
+++ src/groovy/kuorum/web/commands/profile/DomainUserPhoneValidationCommand.groovy
@@ -11,8 +11,20 @@ class DomainUserPhoneValidationCommand {
     String phoneNumberPrefix
     Long phoneNumber
 
+    static List<String> INVALID_PHONES = ["00291", "+291", "00292", "+292", "00299", "+299", "00298", "+298"];
+    static String SPANISH_CODE = "0034"
+
     static constraints = {
-        phoneNumberPrefix nullable:false
-        phoneNumber nullable:false
+        phoneNumberPrefix nullable: false, validator: { val, obj ->
+            if (INVALID_PHONES.contains(val)) {
+                return "CountryNotAllowed"
+            }
+            if (SPANISH_CODE.equals(val)) {
+                if (obj.phoneNumber.toString().length() != 9 && !obj.phoneNumber.toString().startsWith("6")) {
+                    return "NotSpanishPhone"
+                }
+            }
+        }
+        phoneNumber nullable: false
     }
 }
