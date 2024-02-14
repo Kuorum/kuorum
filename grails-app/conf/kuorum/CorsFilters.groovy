520e513f04b0b2e9af306eb2bc10a2cd5453bc2b
Iñaki <inaki.dominguez@kuorum.org>
2024-02-14 10:14:44 +0100

    Added X-Frame-Options on headers

--- grails-app/conf/kuorum/CorsFilters.groovy
+++ grails-app/conf/kuorum/CorsFilters.groovy
@@ -1,10 +1,13 @@
 package kuorum
 
 class CorsFilters {
+
+    String X_FRAME_OPTIONS = "DENY" // DENY | SAMEORIGIN
     def filters = {
         corsFilter(controller:'*', action:'*') {
             before = {
                 response.setHeader("Access-Control-Allow-Origin", "*")
+                response.setHeader("X-Frame-Options", X_FRAME_OPTIONS)
             }
         }
     }
