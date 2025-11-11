85996b696533337a4786c09310da3453ded4819d
PedroR-dev <pedro.ramos@kuorum.org>
2025-11-11 16:00:04 +0100

    Revert "KPV-5971 Fix removing empty p tags on bio save.Added bottom margin to all paragraphs (politician-card p CSS selector)"
    This reverts commit 6a4c408dd97c22e4569f242f1d02512c5d6c7217.

--- grails-app/domain/kuorum/users/KuorumUser.groovy
+++ grails-app/domain/kuorum/users/KuorumUser.groovy
@@ -199,21 +199,7 @@ class KuorumUser {
                 .addProtocols("a", "href", "ftp", "http", "https", "mailto");
         str = Jsoup.clean(str, whitelist);
         str = str.replaceAll("\\n", "")
-        str = removeEmptyParagraphs(str)
         return str;
-    }
-
-    private static removeEmptyParagraphs(String str) {
-        Document doc = Jsoup.parseBodyFragment(str)
-        doc.select("p").each { p ->
-            String pText = p.text()
-            String normalizedText = pText.replace('\u00A0', ' ').trim()
-            if (normalizedText.isEmpty()) {
-                p.remove()
-            }
-        }
-        String strRemovedEmptyParagraphs = doc.body().html()
-        return strRemovedEmptyParagraphs
     }
 
     int hashCode() {
